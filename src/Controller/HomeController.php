fad51196fc1cc9d169297d9176b2929136f2e846
martamazurkiewicz <marta@mazurkiewi.cz>
2021-06-18 10:29:06 +0200

    Adjusting project to be a SPA

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,9 +22,51 @@ class HomeController extends AbstractController
     {
         $em = $this->getDoctrine()->getManager();
         $articles = $em->getRepository(Article::class)->findAll();
+        $recentArticle = $articles[0];
+        $articles = $this->getPairs($articles);
 
         return $this->render('index/index.html.twig', [
+            'article' => $recentArticle,
             'articles' => $articles,
         ]);
+    }
+
+    /**
+     * @Route("/category", name="category")
+     * @param Request $request
+     * @return Response
+     */
+    public function categoryController(Request $request): Response
+    {
+        $articles = $this->getCategorizedArticles($request->query->get('categoryName'));
+        $recentArticle = $articles[0];
+        $articles = $this->getPairs($articles);
+
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'CategoryController',
+            'article' => $recentArticle,
+            'articles' => $articles
+        ]);
+
+    }
+
+    private function getCategorizedArticles(string $categoryName){
+        $em = $this->getDoctrine()->getManager();
+        $category = $em->getRepository(Category::class)->findOneBy(['name_en' => $categoryName]);
+        return $em->getRepository(Article::class)->findBy(['category' => $category]);
+    }
+
+    private function getPairs(array $articles){
+        $size = count($articles);
+        $result = array();
+        for ($i = 0; $i < $size; $i += 2) {
+            if($i+1 < $size) {
+                $result[] = [$articles[$i], $articles[$i + 1]];
+            }
+            else{
+                $result[] = [$articles[$i]];
+            }
+        }
+        return $result;
     }
 }
